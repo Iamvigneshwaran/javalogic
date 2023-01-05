e5c72a5af146cbb173424edfb56e458e90e46943
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-06 00:00:33 +0530

    decimal to binary

--- src/Binarytodecimal.java
+++ src/Binarytodecimal.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Binarytodecimal {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);    
 		System.out.print("Enter a binary number : ");
